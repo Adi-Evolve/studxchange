40cdab577727f55a51fa3de3c10727d9582ebd3f
Adi-Evolve <your-email@example.com>
2025-05-01 02:05:39 +0530

    Resolved merge conflicts

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -6,7 +6,7 @@ const SUPABASE_URL = 'https://ygfjhicakkcctduoscdy.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnZmpoaWNha2tjY3RkdW9zY2R5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5MDgzMDQsImV4cCI6MjA2MDQ4NDMwNH0.pjDlkmoNMpJGxf7viom2hNgWTVEUUz7yYWgMUAaCx8c';
 
 // Initialize Supabase client with additional options for Vercel environment
-const supabase = supabaseClient.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     autoRefreshToken: true,
     persistSession: true
